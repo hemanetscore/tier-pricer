3f0f3af79882dc1f2d64f0d47ba3511ca1cab4d3
Netscore@2020 <hjampula@netscoretech.com>
2021-09-20 12:47:49 +0530

    index page note added

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,18 @@
 import React from "react";
-import { Card } from "@shopify/polaris";
+import { Card, Page } from "@shopify/polaris";
 
 export default class App extends React.Component {
   render() {
     return (
-      <Card title="Online store dashboard" sectioned>
-        <p>View a summary of your online store’s performance.</p>
-      </Card>
+      <Page sectioned>
+        <Card title="After Installation settings" sectioned>
+          <p>
+            Thank you for choosing our app. After installation please set the
+            app status to active. Then add your tier prices as variants named
+            'Rate' or 'rate'. They will appear in the prodcut single page.
+          </p>
+        </Card>
+      </Page>
     );
   }
 }
