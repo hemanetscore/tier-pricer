cffab88f5ac0c300fce19ffac49517f774edd0d8
Netscore@2020 <hjampula@netscoretech.com>
2021-09-20 11:49:39 +0530

    index page changed

--- pages/index.js
+++ pages/index.js
@@ -1,50 +1,12 @@
-import { ResourcePicker } from "@shopify/app-bridge-react";
-import { EmptyState, Page } from "@shopify/polaris";
-import React, { useEffect, useState } from "react";
-import ProductEmptyState from "../components/ProductEmptyState";
-import ProductList from "../components/ProductList";
-import ProductPage from "../components/ProductPage";
-import store from "store-js";
+import React from "react";
+import { Card } from "@shopify/polaris";
 
-function index({ shopOrigin }) {
-  const [isOpen, setIsOpen] = useState(false);
-  const [products, setProducts] = useState([]);
-  const [productsId, setProductsId] = useState([]);
-  useEffect(() => {
-    const productList = store.get(`${shopOrigin}-products`);
-    if (productList) {
-      setProducts(productList);
-    }
-  }, []);
-  useEffect(() => {
-    const ids = products.map((product) => {
-      return {
-        id: product.id,
-      };
-    });
-    setProductsId(ids);
-  }, [products]);
-  function handleProductSelection(payload) {
-    setIsOpen(false);
-    setProducts(payload.selection);
-    store.set(`${shopOrigin}-products`, payload.selection);
+export default class App extends React.Component {
+  render() {
+    return (
+      <Card title="Online store dashboard" sectioned>
+        <p>View a summary of your online store’s performance.</p>
+      </Card>
+    );
   }
-  return (
-    <>
-      <ResourcePicker
-        resourceType="Product"
-        open={isOpen}
-        onCancel={() => setIsOpen(false)}
-        onSelection={handleProductSelection}
-        initialSelectionIds={productsId}
-      />
-      {products.length > 0 ? (
-        <ProductPage setIsOpen={setIsOpen} products={products} />
-      ) : (
-        <ProductEmptyState setIsOpen={setIsOpen} />
-      )}
-    </>
-  );
 }
-
-export default index;
